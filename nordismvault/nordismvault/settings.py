866f642f0c5e68c664fc9f4f611dd5d36bd5eb32
Kai Pietilä <kai.m.pietila@gmail.com>
2021-02-25 21:05:47 +0200

    Add boostrap framework css and js

--- nordismvault/nordismvault/settings.py
+++ nordismvault/nordismvault/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 MEDIA_URL = 'media/'
